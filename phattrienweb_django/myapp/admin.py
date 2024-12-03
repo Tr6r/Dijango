0c284db4b4a20e3564c90b0ee6f01dcd45228dc8
Đinh Quốc Cường <tr6r20@gmail.com>
2024-12-03 16:34:41 +0700

    create method crud articles

--- phattrienweb_django/myapp/admin.py
+++ phattrienweb_django/myapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Articles  # Nhập model Articles từ myapp/models.py
+
+class ArticlesAdmin(admin.ModelAdmin):
+    list_display = ('header', 'user', 'date')  # Hiển thị các trường trong bảng admin
+    search_fields = ('header', 'body')  # Các trường có thể tìm kiếm
+    list_filter = ('user', 'date')  # Bộ lọc cho các trường 'user' và 'date'
+
+# Đăng ký model Articles vào admin
+admin.site.register(Articles, ArticlesAdmin)

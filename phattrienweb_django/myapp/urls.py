14bc397da6a33326d40883abfd9f79de0d94f96a
Trần Thanh Duy <duytranthanh1205@gmail.com>
2024-12-04 00:05:40 +0700

    update login , logout in home

--- phattrienweb_django/myapp/urls.py
+++ phattrienweb_django/myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib import admin
 
 
 urlpatterns = [
@@ -9,4 +10,6 @@ urlpatterns = [
     path("home/", views.home, name="home"),
     path("about/", views.about, name="blog-about"),
     path("", views.home, name="blog-home"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("admin/", admin.site.urls, name="admin"),
 ]
--- phattrienweb_django/phattrienweb_django/urls.py
+++ phattrienweb_django/phattrienweb_django/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from myapp import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('myapp/', include('myapp.urls')),
-    path('', views.home, name='home'),
-
-
+    path("myapp/", include("myapp.urls")),
+    path("", views.home, name="home"),
 ]
